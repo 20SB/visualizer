8c67f2c2f9275e5beca532ad7eedd9278a042b7c
20SB <subhabiswal20@gmail.com>
2024-04-24 21:21:02 +0530

    prepared environment for frontend with chakra ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-    return <div className="App">Visualizer</div>;
+    return (
+        <div className="App">
+            Visualizer
+            <Button colorScheme="blue">Button</Button>
+        </div>
+    );
 }
 
 export default App;
